fa201b4bf0154d5d3c17c3b92dc7b3ed2717088d
joshua inocencio <snotbot1234@gmai.com>
2015-01-14 14:02:56 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -11,3 +11,50 @@ $('document').ready(function(){
     $('p:last').css('color', 'red');
     
 });
+
+$('document').ready(function(){
+    
+    $('#oneButton').bind('click', alertButtonClick);
+    
+    $('#textBox1').bind('blur', onBlurEvent)
+                  .bind('focus', onFocusEvent)
+                  .bind('onmousedown', onMDownEvent)
+                  .bind('onmouseup', onMUpEvent)
+                  .bind('change', onChangeEvent);
+          
+          $(window).resize(resizedWindow);
+});
+
+function alertButtonClick(){
+    alert("there was a button clicked");
+}
+
+function onBlurEvent()
+{
+    $("#second").html("you left the box");
+}
+
+function onFocusEvent()
+{
+    $("#second").html("you entered the box");
+}
+
+function onMDownEvent()
+{
+    $("#second").html("you left the box");
+} 
+
+function onMUpEvent()
+{
+    $("#second").html("you entered the box");
+}
+
+function onChangeEvent()
+{
+    $("#second").html("you changed the box");
+} 
+
+function resizedWindow()
+{
+    $("#second").html("Window was resized W: " + $(window).width() + "H:" + $(window).height());
+} 
