9b0ff728d96eae38da963f91082e518ee1794cbb
joshua inocencio <snotbot1234@gmai.com>
2015-01-20 13:28:18 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -5,12 +5,12 @@
  */
 
 
-$('document').ready(function(){
+/*$('document').ready(function(){
     $('.joshi').css('background-color', 'yellow');
     
-    $('p:last').css('color', 'red');
-    
-});
+    $('p:last').css('color', 'red');    
+});*/
+
 
 $('document').ready(function(){
     
@@ -23,6 +23,10 @@ $('document').ready(function(){
                   .bind('change', onChangeEvent);
           
           $(window).resize(resizedWindow);
+          
+          $('#logo').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+          $('#twoButton').bind('dblClickedMe');
 });
 
 function alertButtonClick(){
@@ -58,3 +62,53 @@ function resizedWindow()
 {
     $("#second").html("Window was resized W: " + $(window).width() + "H:" + $(window).height());
 } 
+
+function mouseOverMe()
+{
+    $("#second").html("you put your cursor on my logo");
+} 
+
+function mouseOutMe()
+{
+    $("#second").html("you stopped touching my logo");
+}
+
+function dbClickedMe()
+{
+    $("#second").html("you double click my button");
+}
+
+
+$('#show').css('visibility', 'hidden');
+
+$('#hide').bind('click', hideThePage);
+
+$('#show').bind('click', showThePage);
+
+function hideThePage(){
+    $('#show').css('visibility', 'visible');
+    $('div').hide('slide', {}, 2500);
+    $('#show').show('fold', {}, 2500);
+}
+
+function showThePage(){
+    $('div').show('fold', {}, 2500);
+    $('#show').hide('puff', {}, 2500);
+}
+
+$("document").ready(function(){
+    
+$('#replaceWHtml').bind('click', replaceWHtml);
+$('#replaceWText').bind('click', replaceWText);
+
+});
+
+function replaceWHtml(){
+    $('#h3Tag').html('<h6>Now I\'m an h6<\h6>');
+
+}
+
+function replaceWText(){
+    $('#h3Tag').text('<h6>Now I\'m an h6<\h6>');
+
+}
